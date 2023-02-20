418fdef79d07cda66a2402ae5c9248668a40f1a9
Danny <dprakonekham@gmail.com>
2023-02-20 01:55:53 -0800

    added a button that changes the size

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const grid = document.getElementById('gContainer')
-
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        const element = document.createElement("div");
-        element.id = "grid" + i + j;
-        element.className = "grid"
-        element.textContent = "."
-        grid.appendChild(element)
+function howBig(num){
+    if(num<101){
+        for(let i = 0; i < num; i++){
+            for(let j = 0; j < num; j++){
+                const element = document.createElement("div");
+                element.id = "grid" + i + j;
+                element.className = "grid"
+                element.textContent = "."
+                grid.appendChild(element)
+            }
+        }
     }
 }
+const gridDimensions = document.querySelector('.gridSize')
+gridDimensions.addEventListener(`click`, function(e){
+    let size = prompt("How Big? (Max 100)");
+    howBig(size)
+});

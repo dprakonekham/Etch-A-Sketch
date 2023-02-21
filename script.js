1fee188d22732167f2d4b50bd222ccd628d56d18
Danny <dprakonekham@gmail.com>
2023-02-21 01:48:23 -0800

    fixed invalid input bug for size

--- script.js
+++ script.js
@@ -21,35 +21,35 @@ for(let i = 0; i < 16; i++){
 }
 
 function howBig(num){
-    if(num<101){
-        for(let i = 0; i < num; i++){
-            for(let j = 0; j < num; j++){
-                const element = document.createElement("div");
-                element.id = "grid" + i + j;
-                element.className = "grid";
-                element.textContent = ".";
-                element.style.color = "lightgrey";
-                element.style.backgroundColor = "lightgrey";
-                element.addEventListener(`mouseover`, function(e){
-                    element.style.color = "black";
-                    element.style.backgroundColor = "black";
-                })
-                grid.appendChild(element);
-            }
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+            const element = document.createElement("div");
+            element.id = "grid" + i + j;
+            element.className = "grid";
+            element.textContent = ".";
+            element.style.color = "lightgrey";
+            element.style.backgroundColor = "lightgrey";
+            element.addEventListener(`mouseover`, function(e){
+                element.style.color = "black";
+                element.style.backgroundColor = "black";
+            })
+            grid.appendChild(element);
         }
     }
 }
 
 const gridDimensions = document.querySelector('.gridSize');
 gridDimensions.addEventListener(`click`, function(e){
-    //Removes every child
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-      }
     let size = prompt("How Big? (Max 100)");
-    gridContainer.style.gridTemplateColumns = "repeat(" + size + ", minmax(0,1fr))";
-    gridContainer.style.gridTemplateRows = "repeat(" + size + ", minmax(0,1fr))";
-    howBig(size);
+    if(size<101){
+        //Removes every child of the previous board
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        gridContainer.style.gridTemplateColumns = "repeat(" + size + ", minmax(0,1fr))";
+        gridContainer.style.gridTemplateRows = "repeat(" + size + ", minmax(0,1fr))";
+        howBig(size);
+    }
 });
 
 const eraseButton = document.querySelector('.eraseBtn');

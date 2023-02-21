782672d99a70c28866ea9f9afe04c78a0e34a2d5
Danny <dprakonekham@gmail.com>
2023-02-21 00:24:29 -0800

    set up initial board

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const grid = document.getElementById('gContainer');
 
+//Initial Board
+const gridContainer = document.querySelector('.gridContainer');
+gridContainer.style.gridTemplateColumns = "repeat(" + 16 + ", minmax(0,1fr))";
+gridContainer.style.gridTemplateRows = "repeat(" + 16 + ", minmax(0,1fr))";
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
+        const element = document.createElement("div");
+        element.id = "grid" + i + j;
+        element.className = "grid";
+        element.textContent = ".";
+        element.style.color = "lightgrey";
+        element.style.backgroundColor = "lightgrey";
+        element.addEventListener(`mouseover`, function(e){
+            element.style.color = "black";
+            element.style.backgroundColor = "black";
+        })
+        grid.appendChild(element)
+    }
+}
+
 function howBig(num){
     if(num<101){
         for(let i = 0; i < num; i++){
@@ -21,10 +41,6 @@ function howBig(num){
 }
 
 const gridDimensions = document.querySelector('.gridSize');
-const gridContainer = document.querySelector('.gridContainer');
-const eraseButton = document.querySelector('.eraseBtn');
-const gridContents = grid.children;
-
 gridDimensions.addEventListener(`click`, function(e){
     //Removes every child
     while (grid.firstChild) {
@@ -36,6 +52,8 @@ gridDimensions.addEventListener(`click`, function(e){
     howBig(size);
 });
 
+const eraseButton = document.querySelector('.eraseBtn');
+const gridContents = grid.children;
 eraseButton.addEventListener(`click`, function(e){
     for(let i = 0; i < gridContents.length; i++){
         gridContents[i].style.color = "lightgrey"
